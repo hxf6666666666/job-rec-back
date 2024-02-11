8d2cfe381ce4c9b30040d4ee1b98ac230a7359df
hxf <635663114@qq.com>
2024-02-11 23:04:33 +0800

    初始化

---
+++ src/main/java/org/example/jobrecback/JobRecBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.jobrecback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobRecBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobRecBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jobrecback/JobRecBackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jobrecback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobRecBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
