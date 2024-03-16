ea6a7bdb106037b8507ba44c9236130fb9603edc
hxf <635663114@qq.com>
2024-03-17 01:46:19 +0800

    实现用户的查询

--- src/main/java/org/example/jobrecback/pojo/EducationExperience.java
+++ src/main/java/org/example/jobrecback/pojo/EducationExperience.java
@@ -3,7 +3,6 @@ package org.example.jobrecback.pojo;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
 
--- src/main/java/org/example/jobrecback/pojo/EmployeeJobExperience.java
+++ src/main/java/org/example/jobrecback/pojo/EmployeeJobExperience.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
 
 import java.time.Instant;
 
